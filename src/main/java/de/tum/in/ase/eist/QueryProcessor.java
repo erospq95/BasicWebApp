bffbdc61aa08146a023f0d10ca83cce40acf1961
erospq <erosparedesq@gmail.com>
2022-07-13 14:44:43 +0200

    Comment added

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -14,7 +14,7 @@ public class QueryProcessor {
         } else if (query.contains("name")) {
            return "MyTeam";
         } else { // TODO extend the programm here
-            // Add a new comment here, please don't remove it
+            // Add a new comment here, please don't remove it please
             return "";
         }
     }
