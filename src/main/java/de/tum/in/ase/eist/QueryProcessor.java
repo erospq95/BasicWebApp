d19529e0ad08b2ab32da9d3477f08d3d6fc3f9c9
erospq <erosparedesq@gmail.com>
2022-07-13 15:11:29 +0200

    Added plus operation

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -7,12 +7,28 @@ public class QueryProcessor {
 
     public String process(String query) {
         query = query.toLowerCase();
+        // query = "what is 12 plus 15"
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
             return "Eros";
+        } else if (query.contains("plus")){
+            // here we get a list of words
+            String[] words = query.split(" ");
+            int sum = 0;
+            for(String word : words){
+                try {
+                    int  number = Integer.parseInt(word);
+                    sum += number;
+                } catch(Exception e){
+                    // ignore
+                }
+            }
+            return String.valueOf(sum);
+        } else if (query.contains("largest")){
+
         }
             // Add a new comment here, please don't remove it please}
         return "";
